18cb599a73dbde69377254ded6e43bf5692f61bb
Caio Fernandes <caiow.wk2@proton.me>
2023-08-06 18:45:02 -0300

    fix: validation for getting product

--- gateway/gateway/service.py
+++ gateway/gateway/service.py
@@ -23,10 +23,6 @@ class GatewayService(object):
     products_rpc = RpcProxy('products')
     
     @memoize_with_ttl(expiration=60)
-    def _get_map_of_products(self):
-        return {prod['id']: prod for prod in self.products_rpc.list()}
-
-    @memoize_with_ttl(expiration=60)
     def _get_product_by_id(self, product_id):
         return self.products_rpc.get(product_id)
 
@@ -112,17 +108,15 @@ class GatewayService(object):
         # raise``OrderNotFound``
         order = self.orders_rpc.get_order(order_id)
 
-        # Retrieve all products from the products service
-        product_map = self._get_map_of_products()
-
         # get the configured image root
         image_root = config['PRODUCT_IMAGE_ROOT']
 
         # Enhance order details with product and image details.
         for item in order['order_details']:
             product_id = item['product_id']
+            product = self._get_product_by_id(product_id)
 
-            item['product'] = product_map[product_id]
+            item['product'] = product
             # Construct an image url.
             item['image'] = '{}/{}.jpg'.format(image_root, product_id)
 
@@ -218,10 +212,11 @@ class GatewayService(object):
 
     def _create_order(self, order_data):
         # check order product ids are valid
-        valid_product_ids = self._get_map_of_products()
-        
         for item in order_data['order_details']:
-            if item['product_id'] not in valid_product_ids:
+            try:
+                product_id = item['product_id']
+                self._get_product_by_id(product_id)
+            except Exception:
                 raise ProductNotFound(
                     "Product Id {}".format(item['product_id'])
                 )
--- gateway/test/interface/test_service.py
+++ gateway/test/interface/test_service.py
@@ -133,7 +133,7 @@ class TestGetOrder(object):
         }
 
         # setup mock products-service response:
-        gateway_service.products_rpc.list.return_value = [
+        gateway_service.products_rpc.get.side_effect = [
             {
                 'id': 'the_odyssey',
                 'title': 'The Odyssey',
@@ -193,7 +193,8 @@ class TestGetOrder(object):
 
         # check dependencies called as expected
         assert [call(1)] == gateway_service.orders_rpc.get_order.call_args_list
-        assert [call()] == gateway_service.products_rpc.list.call_args_list
+        assert [call('the_odyssey'), call('the_enigma')] == gateway_service.products_rpc.get.call_args_list
+
 
     def test_order_not_found(self, gateway_service, web_session):
         gateway_service.orders_rpc.get_order.side_effect = (
@@ -283,22 +284,13 @@ class TestCreateOrder(object):
 
     def test_can_create_order(self, gateway_service, web_session):
         # setup mock products-service response:
-        gateway_service.products_rpc.list.return_value = [
-            {
+        gateway_service.products_rpc.get.return_value = {
                 'id': 'the_odyssey',
                 'title': 'The Odyssey',
                 'maximum_speed': 3,
                 'in_stock': 899,
                 'passenger_capacity': 100
-            },
-            {
-                'id': 'the_enigma',
-                'title': 'The Enigma',
-                'maximum_speed': 200,
-                'in_stock': 1,
-                'passenger_capacity': 4
-            },
-        ]
+            }
 
         # setup mock create response
         gateway_service.orders_rpc.create_order.return_value = {
@@ -321,7 +313,7 @@ class TestCreateOrder(object):
         )
         assert response.status_code == 200
         assert response.json() == {'id': 11}
-        assert gateway_service.products_rpc.list.call_args_list == [call()]
+        assert gateway_service.products_rpc.get.call_args_list == [call("the_odyssey")]
         assert gateway_service.orders_rpc.create_order.call_args_list == [
             call([
                 {'product_id': 'the_odyssey', 'quantity': 3, 'price': '41.00'}
@@ -360,22 +352,8 @@ class TestCreateOrder(object):
         self, gateway_service, web_session
     ):
         # setup mock products-service response:
-        gateway_service.products_rpc.list.return_value = [
-            {
-                'id': 'the_odyssey',
-                'title': 'The Odyssey',
-                'maximum_speed': 3,
-                'in_stock': 899,
-                'passenger_capacity': 100
-            },
-            {
-                'id': 'the_enigma',
-                'title': 'The Enigma',
-                'maximum_speed': 200,
-                'in_stock': 1,
-                'passenger_capacity': 4
-            },
-        ]
+        gateway_service.products_rpc.get.side_effect = side_effect = (
+            ProductNotFound('missing'))
 
         # call the gateway service to create the order
         response = web_session.post(
